fdac270d7f54733ef6180b1db8e99983a652f427
Laurent Labine <laurentlabine@gmail.com>
2020-12-07 14:49:07 +0100

    Checking in preliminary version to test

--- server.js
+++ server.js
@@ -2,8 +2,24 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
+
+const browser = require("browser-detect");
+
+
+
+
+//Request Ip method found here : https://stackfame.com/get-ip-address-node#How_to_User8217s_get_IP_address_in_Express_JS
+const requestIp = require("request-ip");
+
+const ipMiddleware = function(req, res, next) {
+  
+  console.log(clientIp) 
+  next();
+};
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -22,6 +38,17 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/whoami", 
+function (req, res) {
+  const result = browser(req.headers["user-agent"]);
+  const clientIp = requestIp.getClientIp(req);
+  res.json({
+    ipaddress: clientIp,
+    language:req.headers["accept-language"].split(",")[0] +", " + req.headers["accept-language"].split(",")[1],
+    software: result.name + "/ " + result.version
+  });
 });
 
 
